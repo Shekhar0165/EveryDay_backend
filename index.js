ba5977c1ca72251f66954fd6ebe8944491133ab5
shekhar kashyap <shekharkashyap913@gmail.com>
2025-07-22 11:06:50 +0530

    fix some issuse

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ io.on('connection', async (socket) => {
   HandleTrackOrder(socket,io,client);
 });
 
-server.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
+server.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
 });
